cfdbf62b9ee6d92ea536ecf128b2132059fa745d
Seminar Bioinformatik <billyasser@hotmail.co.uk>
2022-02-14 10:54:37 +0100

    2nd exercise added, and not commented on

--- 14022022-1.py
+++ 14022022-1.py
@@ -23,4 +23,26 @@ def rando(size=random.randint(2, 10), chars=string.digits):
 def rando(size=random.randint(2, 10), chars=string.ascii_lowercase + string.digits):
     return "".join(random.choice(chars) for _ in range(size))
 '''
-print(rando())
+# print(rando())
+
+print("We are going to generate 4 lists of random numbers. Would you choose some boundaries for me?")
+
+#get user input
+print("Ok, list 1...")
+list1=list(range(int(input("Lower bound of this range?: ")),int(input("Upper Bound of this range?: ")), int(input("And increment?: "))))
+print(list1)
+
+#do it the other times
+print("Now, list 2...")
+list2=list(range(int(input("Lower bound of this range?: ")), int(input("Upper Bound of this range?: ")), int(input("And increment?: ")) ))
+print(list2)
+
+print("Ok, list 3...")
+list3=list(range(int(input("Lower bound of this range?: ")),int(input("Upper Bound of this range?: ")), int(input("And increment?: ")) ))
+print(list3)
+
+print("Ok, list 4...")
+list4=list(range(int(input("Lower bound of this range?: ")),int(input("Upper Bound of this range?: ")), int(input("And increment?: ")) ))
+print(list4)
+
+print("\nNice! All done!")
