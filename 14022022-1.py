f6fe8a01bd8dc923d40e635aa4d6b51a0b32509f
Bilal <billyasser@hotmail.co.uk>
2022-02-15 18:35:48 +0100

    corrected responses to different invalid answers

--- 14022022-1.py
+++ 14022022-1.py
@@ -31,28 +31,28 @@ print("We are going to generate 4 lists of numbers. Would you choose some bounda
 
 #get user input
 print("Ok, list 1...")
-list1=list(range(int(input("Lower bound of this range?: ")),int(input("Upper Bound of this range?: ")), int(input("And increment?: "))))
+list1=list(range(int(input("Lower bound of this range?: ")),int(input("Upper Bound of this range?: ")) + 1, int(input("And increment?: "))))
 print(list1)
 
 #do it the other times
 print("Now, list 2...")
-list2=list(range(int(input("Lower bound of this range?: ")), int(input("Upper Bound of this range?: ")), int(input("And increment?: ")) ))
+list2=list(range(int(input("Lower bound of this range?: ")), int(input("Upper Bound of this range?: ")) + 1, int(input("And increment?: ")) ))
 print(list2)
 
 print("Ok, list 3...")
-list3=list(range(int(input("Lower bound of this range?: ")),int(input("Upper Bound of this range?: ")), int(input("And increment?: ")) ))
+list3=list(range(int(input("Lower bound of this range?: ")),int(input("Upper Bound of this range?: ")) + 1, int(input("And increment?: ")) ))
 print(list3)
 
 print("Ok, list 4...")
-list4=list(range(int(input("Lower bound of this range?: ")),int(input("Upper Bound of this range?: ")), int(input("And increment?: ")) ))
+list4=list(range(int(input("Lower bound of this range?: ")),int(input("Upper Bound of this range?: ")) + 1, int(input("And increment?: ")) ))
 print(list4)
 
 print("Ok, list 5...")
-list5=list(range(int(input("Lower bound of this range?: ")),int(input("Upper Bound of this range?: ")), int(input("And increment?: ")) ))
+list5=list(range(int(input("Lower bound of this range?: ")),int(input("Upper Bound of this range?: ")) + 1, int(input("And increment?: ")) ))
 print(list5)
 
 print("Ok, list 6...")
-list6=list(range(int(input("Lower bound of this range?: ")),int(input("Upper Bound of this range?: ")), int(input("And increment?: ")) ))
+list6=list(range(int(input("Lower bound of this range?: ")),int(input("Upper Bound of this range?: ")) + 1, int(input("And increment?: ")) ))
 print(list6)
 
 #User is demonstrated what the append and extend methods do
@@ -73,32 +73,34 @@ print("Extension: 2(6)")
 list2.extend(list6)
 print(str(list2))
 
-print("As you can see from our results, appendages add the entire list as an entry")
-print("While extending a list adds the entries of the extended list")
-print("as entries in the new list.")
+print("As the results show, appendages nest an entire list as a last entry,")
+print("while extending a list adds the individual entries of the list added")
+print("as entries in the resulting list.")
 print("NOTE: Our data remain unchanged as the script runs")
 #print("\nNice! All done!")
 
-'''
-"Slicing" is the practice of getting specific entries of a list
-and is demonstrated by
-e.g.
-list2=list(list1[0:8])
-print(str(list2))            # now you have created a new list by slicing and printed it!
-'''
+
+'''"Slicing" is the practice of getting specific entries of a list
+and is demonstrated by'''
+'e.g.'
+print('"Slicing" is the practice of getting specific entries of a list\nand is demonstrated by\ne.g.\nlist2=list(list1[0:8])\nprint(str(list2))"')
+sloice=list(list1[0:8])
+print(str(list1))
+print(str(sloice))            # now you have created a new list by slicing and printed it!
+
 
 #User-defined slice
 
 # User is handed the second of the two most-recently generated lists, and instructed to choose slice points.
-def get_int(FuncList, prompt1, prompt2):
+def get_list_n_slice(FuncList, prompt1, prompt2):
     print("Length of list is " + str(len(FuncList)))
-    slice1 = int(input(prompt1))
-    slice2 = int(input(prompt2))
+
 
     # While loop gets broken when invalid answers are given more than once
     while True:
         try:
-            print("Slicing between value " + str((int(slice1) + 1)) + " and " + str(int(slice2) + 1))
+            slice1 = int(input(prompt1))
+            slice2 = int(input(prompt2))
             # Reject invalid inputs
         except ValueError:
             print("Try again, pal. Whole numbers only. \n")
@@ -107,40 +109,53 @@ def get_int(FuncList, prompt1, prompt2):
         if  int(slice1) <= len(FuncList) and int(slice2) <= len(FuncList):
             print(str(FuncList[slice1:slice2]))
             break
-# Responds to repeated invalid answers
+# Responds differently when different invalid answers are submitted,
+# which can depend on how many times that is done.
         elif slice1 >= len(FuncList) or slice2 >= len(FuncList):
             print("Sorry, you've selected a number larger than the list")
-            slice1 = int(input("Please give the first number again: "))
-            slice2 = int(input("Please give the second: "))
-            if slice1 >= len(FuncList) or slice2 >= len(FuncList):
-                print("I was only patient enough to take invalid numbers one time")
-                break
+            while True:
+                try:
+                    slice3 = int(input("Please give the first number again: "))
+                except ValueError:
+                    print("That wasn't a number.")
+                    continue
+                if slice3 >= len(FuncList):
+                    print("I was only patient enough to take overly large numbers one time.")
+                    return # return stops my function (def) running when cnd met
+                else:
+                    print("Thanks!")
+                    break
+            while True:
+                try:
+                    slice4 = int(input("Please give the second number again: "))
+                except ValueError:
+                    print("That wasn't a number.")
+                    continue
+                if slice4 >= len(FuncList):
+                    print("I was only patient enough to take overly large numbers one time.")
+                    return
+                else:
+                    print(FuncList[slice3:slice4])
+                    print('Notice how it starts on the entry after where you sliced it')
+                    print('with the first number!')
+                    break
+            break
+
+
 # Just to be clear, added an else statement
         else:
             break
 
-''''
-print("Ok, let's do a slice of the most recently printed list.")
-print("Please give me two numbers in ascending order. Lowest should be at least zero.")
-slice1 = int(input("1st one? "))
-slice2 = int(input("2nd one? "))
-print(str(list2[slice1:slice2]))
-
-print("Ok, let's do a slice of the other extended list we printed.")
-print("Again, please give me two numbers in ascending order. Lowest should be at least zero.")
-slice3 = int(input("1st one? "))
-slice4 = int(input("2nd one? "))
-print(str(list1[slice3:slice4]))
-'''
 
 # Function is introduced to user, then called
 print("Ok, let's do a slice of the other extended list we printed.")
 print("Please give me two numbers in ascending order. Lowest should be zero or higher.")
-get_int(list(list2), "1st one? ", "2nd one? ")
+get_list_n_slice(list(list2), "1st one? ", "2nd one? ")
 
-
+'''
 # Demonstration of .remove function
 print("Cool, let's take one out of the other new list. You choose.")
 print("Length of list is " + str(len(list1)))
-list1.remove(int(input("Which shall we remove? Remember that list numbers are funny in Python! ")))
+list1.remove(int(input("Which shall we remove? ")))
 print(str(list1))
+'''
